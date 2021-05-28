f2deeed49db773ba5c581fa331175a2d1bd8c8b6
AngryMoustache <sander.vandamme11@gmail.com>
2021-05-28 16:11:19 +0200

    Initial commit

---
+++ src/TwitterServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AngryMoustache\Twitter;
+
+use Illuminate\Support\ServiceProvider;
+
+class TwitterServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
